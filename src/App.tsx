d2a2f2682dd37bc8772418b17abf10b30e10a305
Kate Fractal <k8fractal@gmail.com>
2022-08-10 18:25:15 -0400

    Rotating cube. Bug: Canvas size increasing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Canvas, useFrame } from '@react-three/fiber'
+import Cube from './Cube';
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
           Tetrahedr
         </p>
         <Canvas>
-        <ambientLight />
+          <ambientLight intensity={0.5}/>
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          
+          <Cube position={[-1,0,0]}/>
         </Canvas>
         <p>After the Canvas</p>
       </header>
--- src/Cube.tsx
+++ src/Cube.tsx
@@ -1,8 +1,21 @@
-import {useRef, useState } from 'react'
-import {ThreeElements, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import { Mesh } from 'three'
+import { MeshProps, useFrame } from '@react-three/fiber'
 
-const Cube = () => {
-    const cubeRef= useRef();
+const Cube = (props: MeshProps) => {
+    const cubeRef= useRef<Mesh>(null); //Initally null, will be set in the object return.
+
+    //by the time this gets called, cubeRef will point to the mesh object.
+    useFrame((state,delta) => (cubeRef.current!.rotation.x += 0.01))
+
+    return (
+        <mesh
+        {...props}
+        ref = {cubeRef} >
+            <boxGeometry args = {[1,1,1]}/>
+            <meshStandardMaterial color='green'/>
+        </mesh>
+    )
 
 };
 
