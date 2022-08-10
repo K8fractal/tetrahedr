473865d60d172e73810c29fa93ca20bfbb3ce29f
Kate Fractal <k8fractal@gmail.com>
2022-08-10 16:12:29 -0400

    Started making cube component

---
+++ src/Cube.tsx
@@ -0,0 +1,9 @@
+import {useRef, useState } from 'react'
+import {ThreeElements, useFrame } from '@react-three/fiber'
+
+const Cube = () => {
+    const cubeRef= useRef();
+
+};
+
+export default Cube;
