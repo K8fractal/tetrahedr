2b5adc3968725a561a1d603b514b4ab58ef4a20b
Kate Fractal <k8fractal@gmail.com>
2022-10-28 17:47:14 -0400

    clean up App code

--- src/App.tsx
+++ src/App.tsx
@@ -15,43 +15,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Tetrahedr</p>
-        <Canvas className="Three-canvas" camera={{ position: [0.1, 0.1, 5.1] }}>
-          <OrbitControls />
-          <ambientLight intensity={0.5} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-          <pointLight position={[-10, -10, -10]} />
+        <>
+          <Canvas
+            className="Three-canvas"
+            camera={{ position: [0.1, 0.1, 5.1] }}
+          >
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
 
-          {/* <Cube position={[0, 1, 0]} scale={1} /> */}
-          {/* <Triangle
-            vertices={[1, 1, 1, 1, -1, 1, 1, 0, 0]}
-            position={[0, 0, 0]}
-          /> */}
-          {/* <IrregularTetrahedron
-            position={[0, 1, 0]}
-            scale={0.5}
-            quaternion={[-SQRT1_2, SQRT1_2, 0, 0]}
-            color="purple"
-          /> */}
-          {/* <IrregularTetrahedron
-            position={[0, 0, 0]}
-            scale={0.5}
-            quaternion={[0.5, -0.5, -0.5, 0.5]}
-            color="green"
-            vertices={[
-              new Vector3(1, 1, 1),
-              new Vector3(0, 1, -1),
-              new Vector3(1, -1, -1),
-              new Vector3(-1, 1, -1),
-            ]}
-          /> */}
-          {/* <Facet
-            key="base"
-            position={[0, 1, 0]}
-            quaternion={[SQRT1_2, -SQRT1_2, 0, 0]}
-          /> */}
-          <FacetStructure />
-        </Canvas>
-        <p>Click on a face to add to the structure.</p>
+            <FacetStructure />
+          </Canvas>
+          <p>Click on a face to add to the structure.</p>
+        </>
         <p className="footnote">
           <a href="https://github.com/K8fractal/tetrahedr">
             Code for Tetrahedr on github
